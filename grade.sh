6e6955b5bbb901254fbe2cc554573edb88b83e0b
andrewtran5222 <andrewtran5222@gmail.com>
2023-02-15 15:57:29 -0800

    Added file check

--- grade.sh
+++ grade.sh
@@ -3,3 +3,11 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples found"
+elif
+    echo "Missing ListExamples file"
+    exit 1
+fi
